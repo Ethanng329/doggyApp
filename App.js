e71ea54c56ec185d579832ded5f698d0762b014f
Ethan Ng <ethanng329@gmail.com>
2019-12-13 15:57:55 +0000

    import react navigation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@
  * @format
  * @flow
  */
+import 'react-native-gesture-handler';
 
 import React from 'react';
 import {
@@ -24,7 +25,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
